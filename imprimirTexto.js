f0fc3241cc95d791e4dc38050a48ab35c4336c6e
Larivelsa <larissavelsa@gmail.com>
2021-04-02 17:46:51 -0300

    inicial 2

--- imprimirTexto.js
+++ imprimirTexto.js
@@ -1,3 +1,8 @@
+var texto = "Hello world!";
+//let texto; o let declara variavel, mas nao eh possivel redeclarar a mesma variavel
+
 function imprimirTexto(textoParaImprimir){
     console.log(textoParaImprimir);
 }
+
+imprimirTexto(texto);
