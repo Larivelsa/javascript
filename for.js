c3eec69099e772459000c32798f3502f6a0e13c2
Larissa <larissavelsa@gmail.com>
2021-04-09 16:51:01 -0300

    repeticao

---
+++ for.js
@@ -0,0 +1,13 @@
+for (let contador = 0; contador < 10; contador++) // escopo do for
+{
+console.log("O contador vale "+contador);
+}
+
+let valor_produto = 500;
+let total_parcelas = 15;
+
+for ( let contador = 1; contador <= total_parcelas; contador++ )
+{
+    let valor_parcela = valor_produto / contador;
+    console.log("Nº de parcelas: " + contador + " - Valor da parcela: R$" + valor_parcela.toFixed(2));
+}
