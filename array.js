a476b98ddb1f7cdd7e5edc67f0b690a6aeec9000
Larivelsa <larissavelsa@gmail.com>
2021-04-11 15:25:22 -0300

    array organizado e comentado

--- array.js
+++ array.js
@@ -1,44 +1,13 @@
-/* o array sem inicia na posicao 0, 
-comporta multiplos tipos de dados,
+/* 
+Array (vetor)
+- O array eh um objeto de alto nivel parecido com listas
+- O array sem objetos inicia na posicao 0
+- Comporta multiplos tipos de dados no mesmo array
+- Nao possui tamanho e tipo fixos
+fonte: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
 */
 
-var larissa = [
-    "programadora",
-    30,
-    "Vargem Grande do Sul",
-    true
-];
-
-
-var meses = [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez"
-];
-
-
-var notas = [
-    10,9,8
-];
-var quantidade = 3;
-var soma = notas[0] + notas[1] + notas[2];
-var media = soma / quantidade;
-
-console.log("Média: " + media);
-console.log("A coleção do array notas: " + notas);
-notas[2]=9;
-console.log("A coleção do array notas: " + notas);
-
-
+/* array signos */
 var signos = [
     "Áries",
     "Touro",
@@ -54,63 +23,48 @@ var signos = [
     "Peixes"
 ];
 
-console.log(signos.length);
+console.log("Array 'signos' completo: "+signos); // exibe o array completo
+console.log("Quantidade de itens do array 'signos': "+signos.length);/* retorna o tamanho (ultimo indice +1) ou altera o array */
+console.log("Posicao 11 do array 'signos' contem: "+signos[11]);
 
-var frases = [
-    "Sorte é o resultado de muito esforço, trabalho e dedicação",
-    "Imagine uma nova história para sua vida e acredite nela",
-    "Pedras no caminho? Eu guardo todas. Um dia vou construir um castelo",
-    "Se existe uma forma de fazer melhor, descubra-a",
-    "Seja a mudança que você deseja ver no mundo.",
-    "Um objetivo nada mais é do que um sonho com limite de tempo",
-    "Você precisa fazer aquilo que pensa que não é capaz de fazer",
-    "A persistência é o caminho do êxito.",
-    "Concentre-se naquilo que você é bom, delegue todo o resto",
-    "A amizade duplica as alegrias e divide as tristezas",
-    "O que quer que você lute, você fortalece, e o que você resiste, persiste",
-    "Lembre-se: sua verdadeira força vem de dentro.",
-    "Nada acontece a menos que sonhemos antes",
-    "Para quem ama, qualquer sacrifício é alegria",
-    "A paz é a única forma de nos sentirmos realmente humanos",
-    "Não existe um caminho para a felicidade. A felicidade é o caminho",
-    "O amor é a alegria acompanhada da ideia de uma causa exterior",
-    "A criatividade é ilimitada, pois a arte só precisa de motivação",
-    "Fé, duas letras que podem mudar sua vida, acredite",
-    "Que a vontade de vencer seja minha maior motivação",
-    "Nunca subestime o poder de sua visão para mudar o seu mundo",
-    "Não coloque limites em seus sonhos. Coloque fé",
-    "Tenho certeza de que se eu sorrisse menos teria menos amigos",
-    "Não espere por uma crise para descobrir o que é importante em sua vida",
-    "Quem sorri sem parar não é alegre, é falso",
-    "A alegria de fazer o bem é a única felicidade verdadeira",
-    "A alegria evita mil males e prolonga a vida",
-    "Coloque fé onde falta coragem",
-    "Levanta, sacode a poeira e dá a volta por cima",
-    "O que vale a pena possuir, vale a pena esperar",
-    "A esperança é o sonho do homem acordado"
-];
+/* array notas */
+var notas=[10,9.5,8,9,8.5];
+console.log("Quantidade de itens no array 'notas': "+notas.length); 
+var ultimo = notas[notas.length -1]; /* notas.length retorna o valor 
+que eh a quantidade de elementos presentes no vetor (no caso, 12) 
+e, ao substrair menos um, eh possivel acessar o ultimo elemento 
+deste array (no caso 11), portanto seria possivel tambem acessar 
+o penultimo, antepenultimo, etc..*/
+var penultimo = notas[notas.length -2]; 
+console.log("Obtem o penultimo elementos do array 'notas': "+penultimo);
+console.log("Obtem o ultimo elementos do array 'notas': "+ultimo);
 
-var data = new Date(); // obtem a data neste formato 2021-04-07T19:27:29.411Z
-var dia = data.getDate(); //obtem o dia da data no objeto, no caso imprime 7
-//var dia = 31;
-console.log("Numero sorteado: "+dia--);
+notas.push(6); /* adiciona elemento no final do array e retorna o novo tamanho do array */
+console.log("Adiciona elemento no final do array 'notas': "+notas);
 
-/* a decrementacao eh necessaria devido a necessidade de o dia ser sempre -1
+notas.pop(6); /* remove elemento no final do array e retorna esse elemento, 
+se o array estiver vazio, retorna udefined e o array nao eh modificado */
+console.log("Remove elemento no final do array 'notas': "+notas);
 
-caso o indice dia seja o dia 31 (obtido no objeto Date)
-havera um erro de undefined, pois o array, embora tenha 31 elementos
-o indice natural do array inicia-se em 0 e termina em 30
-portanto existe a necessidade da decrementacao (dia--)
+notas.shift(); /* remove elemento do inicio do array - sem parametros e retorna esse elemento, 
+se o array estiver vazio, retorna undefined e o array nao eh modificado */
+console.log("Remove elemento do inicio do array 'notas': "+notas);
 
-por exemplo: 
-indice dia 1 (obtido no objeto Date) deve ter o valor 0 (para acessar corretamente o array)
-indice dia 2 (obtido no objeto Date) deve ter o valor 1 (para acessar corretamente o array)
-indice dia 3 (obtido no objeto Date) deve ter o valor 2 (para acessar corretamente o array)
-.
-.
-.
-indice dia 31 deve ter o valor 30 (para acessar corretamente o array)
-*/
-console.log("Variavel dia vale: "+dia);
+notas.unshift(7.5); /* adiciona elemento no inicio do array - com parametros 
+e retorna o novo tamanho do array*/
+console.log("Adiciona elemento no inicio do array 'notas': "+notas);
 
-console.log(frases[dia]);
+console.log("Posicao do elemento 8.5 no array 'notas': "+notas.indexOf(8.5)); /* procura o indice 
+de um elemento no array, se nao encontra, retorna -1 */
+/* .splice(parametros) - apos determinado indice, remove determinado numero de elementos do array e retorna eles
+no caso abaixo, removeu 3 elementos apos o indice 0 */
+console.log("Elementos removidos do array 'notas': "+notas.splice(0,3));
+
+/* .splice() - retorna a copia do array */
+console.log("Copia do array 'notas': "+notas.slice());
+
+
+
+
+
+
