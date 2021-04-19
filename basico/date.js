e6e0ea1474e90c2f509b968b2a20342aad898dc3
Larissa <larissavelsa@gmail.com>
2021-04-19 12:32:40 -0300

    trabalhando com date

--- basico/date.js
+++ basico/date.js
@@ -8,7 +8,7 @@
 const hoje = new Date();
 console.log(hoje);
 
-const data = new Date(1991,0,12,14,45,0,0);
+const data = new Date(2021, 3, 18, 14, 45, 0, 0);
 console.log(data);
 
 /** UTC (Tempo Universal Coordenado) - referencia para calcular fusos horarios
@@ -17,14 +17,35 @@ console.log(data);
 */
 console.log(hoje.toString());
 console.log(hoje.toUTCString());
-console.log(hoje.valueOf());//timestamp
+console.log(hoje.valueOf());//timestamp - valor primitivo do Date()
 
 /* obtendo a diferenca de tempo */
 const time_hoje = hoje.getTime();
-const time_data = data.getTime(); 
-
+const time_data = data.getTime();
 const diferenca = time_hoje - time_data;
 const segundos = diferenca / 1000;
 const minutos = segundos / 60;
 const horas = minutos / 60;
-console.log(horas);
+const dias = horas / 24;
+console.log(dias);
+
+/* getters de Date() */
+console.log(hoje.getFullYear());
+console.log(hoje.getMonth());
+console.log(hoje.getDate());//dia do mes
+console.log(hoje.getDay());//dia da semana (dom 0, seg 1, ter 2, qua 3, qui 4, sex 5, sab 6)
+console.log(hoje.getMinutes());
+console.log(hoje.getMilliseconds());
+console.log(hoje.getTime());//representa quantos milissegundos se passaram desde 1970-01-0 00:00:00 ate a data definida
+console.log(hoje.getTimezoneOffset());//retorna a diferenca de fuso horario local em relacao ao UTF
+
+/* getters de conversao de Date() */
+console.log(hoje.toString());//string com data e hora
+console.log(hoje.toDateString());//string com apena a data
+console.log(hoje.toTimeString());//string apenas com a hora
+
+
+
+
+
+
